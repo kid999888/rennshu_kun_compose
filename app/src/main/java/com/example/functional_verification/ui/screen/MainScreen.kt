c183303364f54bd61cf8c3f1030da8327bc02951
XIAOCHEN WANG <Wang.Xiaochen-N175@ma.nid.co.jp>
2024-07-31 17:23:16 +0900

    DashboardScreen,HomeScreen,NotificationsScreen追加

--- app/src/main/java/com/example/functional_verification/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/functional_verification/ui/screen/MainScreen.kt
@@ -1,8 +1,37 @@
 package com.example.functional_verification.ui.screen
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.functional_verification.ui.navigation.BottomNavigation
+import com.example.functional_verification.ui.screen.dashboard.DashboardScreen
+import com.example.functional_verification.ui.screen.home.HomeScreen
+import com.example.functional_verification.ui.screen.notifications.NotificationsScreen
 
+@Preview(showBackground = true, widthDp = 415, heightDp = 923)
 @Composable
 fun MainScreen() {
-    // 这里放置你的主屏幕 Compose 代码
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigation(navController) }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "home",
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable("dashboard") { DashboardScreen() }
+            composable("home") { HomeScreen() }
+            composable("notifications") { NotificationsScreen() }
+        }
+    }
 }
+
+
+
+
---
+++ app/src/main/java/com/example/functional_verification/ui/screen/notifications/NotificationsScreen.kt
@@ -0,0 +1,11 @@
+package com.example.functional_verification.ui.screen.notifications
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.functional_verification.ui.components.CenteredText
+
+@Preview(showBackground = true, widthDp = 415, heightDp = 923)
+@Composable
+fun NotificationsScreen() {
+    CenteredText("Notifications")
+}

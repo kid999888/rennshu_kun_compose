bf8a8a13ba75472fcb871489d4ae3bf4bb75adaa
kid999888 <kzfc523@gmail.com>
2024-11-12 22:23:05 +0900

    バーコードスキャン機能追加

--- app/src/main/java/com/example/functional_verification/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/functional_verification/ui/screen/home/HomeScreen.kt
@@ -5,14 +5,13 @@ import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.functional_verification.ui.screen.home.viewModel.HomeViewModel
 import org.koin.androidx.compose.koinViewModel
 
 @Preview(showBackground = true, widthDp = 415, heightDp = 923)
 @Composable
 fun HomeScreen() {
-    //val viewModel = koinViewModel<HomeViewModel>()
+    val viewModel = koinViewModel<HomeViewModel>()
     //val viewModel: HomeViewModel = viewModel()
     //    CenteredText("Home")
     MessageList(messages = listOf("A->", "B->", "C->", "D->", "E->", "F->", "G->"))
